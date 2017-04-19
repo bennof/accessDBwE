bb79a35015ce633676f28e922f1176e694bdca30
Benjamin Falkner <Benjamin Falkner>
2017-04-19 20:36:24 +0200

    go fmt

--- accessdbwe.go
+++ accessdbwe.go
@@ -5,7 +5,6 @@
 // code is based on some c experiments (more efficient less platform independent)
 // feel free to implement this code in CGO
 
-
 /** mdb.c
 #include <stdio.h>
 #include <stdlib.h>
@@ -125,109 +124,123 @@ int main (int argc, char **argv)
 }
 **/
 
-
 package accessdbwe
 
 import (
+	"database/sql"
+	"encoding/binary"
+	"errors"
+	_ "github.com/mattn/go-adodb"
 	"io"
 	"os"
-	"errors"
-	"encoding/binary"
 	"strings"
-	"database/sql"
-	_ "github.com/mattn/go-adodb" 
 )
 
 //
 // Implementing decoding of the so called password
 //
 
-
 // setting XOR Values
-var JET3_XOR = []byte { 0x86,0xfb,0xec,0x37,0x5d,0x44,0x9c,0xfa,0xc6,
-                        0x5e,0x28,0xe6,0x13,0xb6,0x8a,0x60,0x54,0x94}
-var JET4_XOR = []byte { 0x6a,0xba,0x37,0xec,0xd5,0x61,0xfa,0x9c,0xcf,0xfa,
-                        0xe6,0x28,0x27,0x2f,0x60,0x8a,0x05,0x68,0x36,0x7b,
-                        0xe3,0xc9,0xb1,0xdf,0x65,0x4b,0x43,0x13,0x3e,0xf3,
-                        0x33,0xb1,0xf0,0x08,0x5b,0x79,0x24,0xae,0x2a,0x7c}
+var JET3_XOR = []byte{0x86, 0xfb, 0xec, 0x37, 0x5d, 0x44, 0x9c, 0xfa, 0xc6,
+	0x5e, 0x28, 0xe6, 0x13, 0xb6, 0x8a, 0x60, 0x54, 0x94}
+var JET4_XOR = []byte{0x6a, 0xba, 0x37, 0xec, 0xd5, 0x61, 0xfa, 0x9c, 0xcf, 0xfa,
+	0xe6, 0x28, 0x27, 0x2f, 0x60, 0x8a, 0x05, 0x68, 0x36, 0x7b,
+	0xe3, 0xc9, 0xb1, 0xdf, 0x65, 0x4b, 0x43, 0x13, 0x3e, 0xf3,
+	0x33, 0xb1, 0xf0, 0x08, 0x5b, 0x79, 0x24, 0xae, 0x2a, 0x7c}
 
-func readEncoding(filen string) (string,error) {
+func readEncoding(filen string) (string, error) {
 	file, err := os.Open(filen)
-	if err != nil {return "", err}
+	if err != nil {
+		return "", err
+	}
 	defer file.Close()
-	
-	p, err := readPage(file)
-	if err != nil {return "", err}
 
-	if p[0] != 0 {return "", errors.New("ERROR no vaild db")}
-	
-	buf := make([]byte,20)
+	p, err := readPage(file)
+	if err != nil {
+		return "", err
+	}
+
+	if p[0] != 0 {
+		return "", errors.New("ERROR no vaild db")
+	}
+
+	buf := make([]byte, 20)
 	switch binary.LittleEndian.Uint32(p[0x14:0x18]) {
-		case 0x0:     //Jet3
-			for i:=0; i<18; i++ {
-				buf[i] = p[0x42+i] ^ JET3_XOR[i]
+	case 0x0: //Jet3
+		for i := 0; i < 18; i++ {
+			buf[i] = p[0x42+i] ^ JET3_XOR[i]
+		}
+
+	case 0x1, 0x02, 0x0103: //Jet4
+		// sorry messed up byteorder
+		p[0x66] = p[0x66] ^ JET4_XOR[36+1]
+		p[0x67] = p[0x67] ^ JET4_XOR[36]
+		for i := 0; i < 18; i++ {
+			p[0x42+2*i] = p[0x42+2*i] ^ JET4_XOR[2*i+1]
+			p[0x42+2*i+1] = p[0x42+2*i+1] ^ JET4_XOR[2*i]
+
+			if p[0x42+2*i+1] > 0 {
+				p[0x42+2*i] = p[0x42+2*i] ^ p[0x66]
+				p[0x42+2*i+1] = p[0x42+2*i+1] ^ p[0x67]
 			}
-			
-		case 0x1,0x02,0x0103:     //Jet4
-		 // sorry messed up byteorder
-			p[0x66] = p[0x66] ^ JET4_XOR[36+1]
-			p[0x67] = p[0x67] ^ JET4_XOR[36  ]
-			for i:=0; i<18; i++ {
-				p[0x42+2*i  ] = p[0x42+2*i  ] ^ JET4_XOR[2*i+1]
-				p[0x42+2*i+1] = p[0x42+2*i+1] ^ JET4_XOR[2*i  ]
-				
-				if p[0x42+2*i+1] > 0 { 
-						p[0x42+2*i  ] = p[0x42+2*i  ] ^ p[0x66]
-						p[0x42+2*i+1] = p[0x42+2*i+1] ^ p[0x67]
-				}
-				buf[i] = p[0x42+2*i]
-			}
-		//case 0x02:    //ACCDB2007  //should use same encoding as JET4
-		//case 0x0103:  //ACCDB2010
-		default:
-			return "", errors.New("ERROR unknown encoding")
+			buf[i] = p[0x42+2*i]
+		}
+	//case 0x02:    //ACCDB2007  //should use same encoding as JET4
+	//case 0x0103:  //ACCDB2010
+	default:
+		return "", errors.New("ERROR unknown encoding")
 	}
 
 	var i int
-	for i=0; i<20; i++ {
-		if buf[i]==0 {break}
+	for i = 0; i < 20; i++ {
+		if buf[i] == 0 {
+			break
+		}
 	}
 	return string(buf[:i]), nil
 
 }
 
 func readPage(r io.Reader) ([]byte, error) {
-	p := make([]byte,4096)
+	p := make([]byte, 4096)
 	s, err := r.Read(p)
-	if err != nil {return nil,err}
-	if s != 4096 {return p,errors.New("ERROR incomplete page")} 
-	return p,nil
+	if err != nil {
+		return nil, err
+	}
+	if s != 4096 {
+		return p, errors.New("ERROR incomplete page")
+	}
+	return p, nil
 }
 
 //
 // Implementing new open function
-// 
-func Open(driver, filen string) (*sql.DB, error){
+//
+func Open(driver, filen string) (*sql.DB, error) {
 	var err error
 	var db *sql.DB
 
 	// extract filename from filen string
-	toks := strings.Split(filen,";")
-	for _,tok := range toks {
-		pair := strings.Split(tok,"=")
-		if len(pair)>1 && pair[0] == "Data Source" {
+	toks := strings.Split(filen, ";")
+	for _, tok := range toks {
+		pair := strings.Split(tok, "=")
+		if len(pair) > 1 && pair[0] == "Data Source" {
 			enc, err := readEncoding(pair[1])
 			if err != nil {
-				return nil,err
+				return nil, err
 			}
 			filen = filen + `;Jet OLEDB:Database Password=` + enc + `;`
 		}
 	}
 
 	db, err = sql.Open("adodb", filen)
-	if err != nil {return nil,err}
+	if err != nil {
+		return nil, err
+	}
 
 	err = db.Ping()
-	if err != nil {return nil,err}
-	return db,nil
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
 }
